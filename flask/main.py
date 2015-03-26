90fa9b61261c98f86029e65c4bbbd567e62ded8d
colindmurray <murray.colin43@gmail.com>
2015-03-26 14:11:42 -0500

    small revisions to main page.

--- flask/main.py
+++ flask/main.py
@@ -1,44 +1,38 @@
-from flask import Flask, render_template, url_for, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
 
 @app.route('/')
 def index():
     return render_template('Index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('Groups.html')
+
 
 @app.route('/group/<name>')
 def group(name=None):
     if name == 'alkali':
         return render_template('alkaliLayout.html', name=name)
-    elif name =='alkaline-earth':
+    elif name == 'alkaline-earth':
         return render_template('alkalinearthLayout.html', name=name)
-    elif name =='halogen':
+    elif name == 'halogen':
         return render_template('halogenLayout.html', name=name)
     else:
         return "Page not found!"
+
 
 @app.route('/element/<name>')
 def element(name=None):
     if name == 'helium':
         return render_template('Helium.html', name=name)
-    elif name =='hydrogen':
+    elif name == 'hydrogen':
         return render_template('Hydrogen.html', name=name)
-    elif name =='fluorine':
+    elif name == 'fluorine':
         return render_template('Fluorine.html', name=name)
     else:
         return "Page not found!"
@@ -55,18 +49,6 @@ def period(name=None):
     else:
         return "Page not found!"
 
-# @app.route('/css/<name>')
-# def sstatic(name=None):
-#     return send_from_directory('css', filename=name)
-#
-# @app.route('/images/<name>')
-# def images(name=None):
-#     return send_from_directory('images', filename=name)
-
-# @app.route('/scripts/<name>')
-# def scripts(name=None):
-#     return send_from_directory('scripts', filename=name)
-
 
 if __name__ == '__main__':
-    app.run()#//host='0.0.0.0')
+    app.run()#host='0.0.0.0', port=80)
