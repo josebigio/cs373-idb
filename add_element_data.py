fcf25dad1b0a02fc9e19d37793797b581348dc90
colindmurray <murray.colin43@gmail.com>
2015-04-05 22:49:22 -0500

    modified data loading tool to do more error checking

--- add_element_data.py
+++ add_element_data.py
@@ -15,24 +15,27 @@ def main():
         symbol = e.symbol
         try:
             desc = open(curdir + "elements_info/general_info/%s%s" % (symbol.strip(), ".txt"))
-            trivia = open(curdir + "elements_info/trivia/%s%s" % (symbol.strip(), ".txt"))
-        except IOError e:
+        except IOError as e:
             print("Missing descrption on element: " + symbol)
             print(e)
-            
-        desc_text = desc.read()
-        trivia_text = trivia.read()
-        e.description = desc_text
-        if trivia_text is not None and trivia_text != "":
-            trivia_list = getTrivia(trivia_text)
-        db.session.add(e)
-        db.session.commit()
-        print("Element: " + str(i) + " has " + str(len(trivia_list)) + " snippets of trivia.")
-        if trivia_text is not None and trivia_text != "":
-            for trivia_snippet in trivia_list:
-                t = models.Trivia(description=trivia_snippet, element_number=i)
-                db.session.add(t)
-                db.session.commit()
+        else:
+            desc_text = desc.read()
+            e.description = desc_text
+            db.session.add(e)
+            db.session.commit()
+        try:
+            trivia = open(curdir + "elements_info/trivia/%s%s" % (symbol.strip(), ".txt"))
+        except IOError as e:
+            print("Missing trivia for element: " + symbol)
+            print(e)
+        finally:
+            trivia_text = trivia.read()
+            if trivia_text is not None and trivia_text != "":
+                trivia_list = getTrivia(trivia_text)
+                for trivia_snippet in trivia_list:
+                    t = models.Trivia(description=trivia_snippet, element_number=i)
+                    db.session.add(t)
+                    db.session.commit()
 
 
 
