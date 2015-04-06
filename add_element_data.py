8b4d14b983aa324d7de8321345c85861b82d4e2f
colindmurray <murray.colin43@gmail.com>
2015-04-05 23:23:54 -0500

    added supper duper good tollerance for odd things in the file names

--- add_element_data.py
+++ add_element_data.py
@@ -1,5 +1,5 @@
 from app import db, models
-import re
+import re, os
 
 def getTrivia(trivia):
     pattern = re.compile(r'@@(.+?)##', flags=re.DOTALL)
@@ -15,11 +15,19 @@ def stripWikiCitations(str):
 
 def main():
     curdir = "/home/elementalists/cs373-idb/"
+    generalinfo = "elements_info/general_info/"
     for i in range(1, 119):
         e = models.Element.query.get(i)
         symbol = e.symbol
         try:
-            desc = open(curdir + "elements_info/general_info/%s%s" % (symbol.strip(), ".*"))
+            desc_file = ''
+            for filename in os.listdir(curdir + generalinfo):
+                if filename.startswith(symbol):
+                    desc_file = filename
+            if desc_file == '':
+                desc = open(curdir + "elements_info/general_info/%s%s" % (symbol.strip(), ".txt"))
+            else:
+                desc = open(curdir + generalinfo + desc_file)
         except IOError as e:
             print("Missing descrption on element: " + symbol)
             print(e)
@@ -29,7 +37,14 @@ def main():
             db.session.add(e)
             db.session.commit()
         try:
-            trivia = open(curdir + "elements_info/trivia/%s%s" % (symbol.strip(), ".*"))
+            triv_file = ''
+            for filename in os.listdir(curdir + generalinfo):
+                if filename.startswith(symbol):
+                    triv_file = filename
+            if desc_file == '':
+                trivia = open(curdir + "elements_info/trivia/%s%s" % (symbol.strip(), ".txt"))
+            else:
+                trivia = open(curdir + generalinfo + triv_file)
         except IOError as e:
             print("Missing trivia for element: " + symbol)
             print(e)
