78df7901786e09f3e31a85ecfa8967c8227afcc8
colindmurray <murray.colin43@gmail.com>
2015-04-06 01:20:12 +0000

    changed insert script

--- add_element_data.py
+++ add_element_data.py
@@ -9,22 +9,30 @@ def getTrivia(trivia):
 
 
 def main():
+    curdir = "/home/elementalists/cs373-idb/"
     for i in range(1, 119):
         e = models.Element.query.get(i)
         symbol = e.symbol
-        desc = open("element_info/general_info/%s%s" % (symbol, ".txt"))
-        trivia = open("element_info/trivia/%s%s" % (symbol, ".txt"))
-        desc_text = desc.readlines()
-        trivia_text = trivia.readlines()
+        try:
+            desc = open(curdir + "elements_info/general_info/%s%s" % (symbol.strip(), ".txt"))
+            trivia = open(curdir + "elements_info/trivia/%s%s" % (symbol.strip(), ".txt"))
+        except IOError e:
+            print("Missing descrption on element: " + symbol)
+            print(e)
+            
+        desc_text = desc.read()
+        trivia_text = trivia.read()
         e.description = desc_text
-        trivia_list = getTrivia(trivia_text)
+        if trivia_text is not None and trivia_text != "":
+            trivia_list = getTrivia(trivia_text)
         db.session.add(e)
-        db.commit()
+        db.session.commit()
         print("Element: " + str(i) + " has " + str(len(trivia_list)) + " snippets of trivia.")
-        for trivia_snippet in trivia_list:
-            t = models.Trivia(description=trivia_snippet, element_number=i)
-            db.session.add(t)
-            db.commit()
+        if trivia_text is not None and trivia_text != "":
+            for trivia_snippet in trivia_list:
+                t = models.Trivia(description=trivia_snippet, element_number=i)
+                db.session.add(t)
+                db.session.commit()
 
 
 
