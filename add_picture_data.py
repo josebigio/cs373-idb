e85dc1e36c341650d33adccf89dd6283b6b66c27
colindmurray <murray.colin43@gmail.com>
2015-04-07 11:36:57 -0500

    fixed issue with not stripping image name, may need to fix database

--- add_picture_data.py
+++ add_picture_data.py
@@ -9,23 +9,29 @@ def main():
     for i in range(1, 119):
         firstImageInFolder = True
         e = models.Element.query.get(i)
-        element_name = e.element
+        element_name = e.element.strip()
         element_dir_name = ''
+        print("element: " + element_name)
         for filename in os.listdir(curdir):
+            print("checking : " + filename)
             if filename.lower().startswith(element_name.lower()):
+                print("Found Dir: " + curdir+filename)
                 element_dir_name = filename
         if element_dir_name != '':
             for imagefilename in os.listdir(curdir+element_dir_name):
+                print("--- Examining: " + imagefilename)
                 path = staticimagespath + imagefilename
+                print(path)
                 imageType = None
                 if firstImageInFolder:
+                    print("-- default --")
                     imageType = default
                     firstImageInFolder = False
-                picture = models.Pcitures(image_path=path, element_number=i, image_type=imageType)
-                db.session.add(picture)
+                image = models.Image(image_path=path, element_number=i, image_type=imageType)
+                db.session.add(image)
         else:
             print("Could not find image folder for " + element_name)
-        db.session.commit()
+    db.session.commit()
 
 
 if __name__ == "__main__":
