d23763aa89a7d49d25e2823fd9f823c112d45255
josebigio <sirownsalot@gmail.com>
2015-04-09 01:57:11 +0000

    added more api calls

--- app/views.py
+++ app/views.py
@@ -11,12 +11,37 @@ def index():
 
 
 @app.route('/api/<name>')
-@app.route('/api/element/<name>')
-def api_handling(name):
     if name == 'element':
         return handle_element()
+    elif name == 'period':
+        return handle_period()
+    elif name == 'group':
+        return handle_group()
     else:
-        return handle_individual_element(name)
+        return "Invalid API call"
+
+@app.route('/api/period/<name>')
+def handle_individual_period(name):    
+    period_id = 0
+    try:
+        period_id = int(name)
+    except:
+        return "Invalid period_id"
+
+    period = Element.query.get(period_id)
+    result_list = []    
+
+    column_names = []
+    for c in Period.__table__.columns:
+        column_names.append(str(c).split("periods.")[1])
+        
+    d = dict()
+    for c_name in column_names:
+        d[c_name] = period.__dict__[c_name]
+
+    result_list.append(d)
+    return json.dumps(result_list)
+
 
 
 @app.route('/models/<name>')
@@ -105,4 +130,36 @@ def handle_individual_element(element_symbol):
         d[c_name] = element.__dict__[c_name]
 
     result_list.append(d)
+    return json.dumps(result_list)
+
+def handle_period():
+    periods = list(Period.query.all())
+    result_list = []
+    column_names = []
+    for c in Period.__table__.columns:
+        column_names.append(str(c).split("periods.")[1])
+
+    for period in periods:
+        d = dict()
+        for c_name in column_names:
+            d[c_name] = period.__dict__[c_name]
+        result_list.append(d)
+ 
+
+    return json.dumps(result_list)
+
+def handle_group():
+    groups = list(Group.query.all())
+    result_list = []
+    column_names = []
+    for c in Group.__table__.columns:
+        column_names.append(str(c).split("groups.")[1])
+
+    for group in groups:
+        d = dict()
+        for c_name in column_names:
+            d[c_name] = group.__dict__[c_name]
+        result_list.append(d)
+ 
+
     return json.dumps(result_list)
