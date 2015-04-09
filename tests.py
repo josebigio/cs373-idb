452cc5f16adfa6e86e8d77c003256bd68163b220
colindmurray <murray.colin43@gmail.com>
2015-04-08 22:13:56 -0500

    added elements correctly to tests

--- tests.py
+++ tests.py
@@ -172,18 +172,18 @@ class TestCase(unittest.TestCase):
 
     def test_api_element(self):
         with self.app as c:
-            element = Element(atomic_number=3,symbol='Li',name="Lithium",atomic_mass=6.94,history="w/e")
+            element = Element(atomic_number=1,symbol='H',element="Hydrogen",phase="phase",most_stable_crystal="msc",type="type",ionic_radius=1.1,atomic_radius=1.2,electronegativity=2.0,first_ionization_potential=3.0,density=1.0,melting_point_k=100.100,boiling_point_k=100.100,isotopes=4,discoverer="Downing",year_of_discovery=100,specific_heat_capacity=100.100,electron_configuration="electron_configuration",description="description")
             db.session.add(element)
             db.session.commit()
-            resp = c.get('/api/element/Li')
+            resp = c.get('/api/element/Hi')
             data = json.loads(resp.data)[0]
-            self.assert_equal(data['element'].lower(), 'hydrogen')
+            self.assert_equal(data['element'], 'Hydrogen')
             self.assert_equal(data['atomic_number'], 1)
             self.assert_equal(data['symbol'], 'H')
 
     def test_api_elements(self):
         with self.app as c:
-            element = Element(atomic_number=3,symbol='Li',name="Lithium")
+            element = Element(atomic_number=1,symbol='H',element="Hydrogen",phase="phase",most_stable_crystal="msc",type="type",ionic_radius=1.1,atomic_radius=1.2,electronegativity=2.0,first_ionization_potential=3.0,density=1.0,melting_point_k=100.100,boiling_point_k=100.100,isotopes=4,discoverer="Downing",year_of_discovery=100,specific_heat_capacity=100.100,electron_configuration="electron_configuration",description="description")
             db.session.add(element)
             db.session.commit()
             resp = c.get('/api/element/')
