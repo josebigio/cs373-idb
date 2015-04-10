2fa635a0253e7c2a8d7e20bbca7f2af9761d4a6c
josebigio <sirownsalot@gmail.com>
2015-04-09 20:33:04 -0500

    changed directory structure

---
+++ db_generation/add_element_data.py
@@ -0,0 +1,66 @@
+from app import db, models
+import re, os
+
+def getTrivia(trivia):
+    pattern = re.compile(r'@@(.+?)##', flags=re.DOTALL)
+    result = pattern.findall(trivia)
+    return result
+
+
+def stripWikiCitations(str):
+    pattern = re.compile(r'\[.{1,3}\]')
+    return re.sub(pattern, '', str)
+
+
+
+def main():
+    curdir = "/home/elementalists/cs373-idb/"
+    generalinfo = "elements_info/general_info/"
+    triviapath = "elements_info/trivia/"
+    for i in range(1, 119):
+        e = models.Element.query.get(i)
+        symbol = e.symbol
+        try:
+            desc_file = ''
+            for filename in os.listdir(curdir + generalinfo):
+                if filename.startswith(symbol.strip()+".txt"):
+                    desc_file = filename
+            if desc_file == '':
+                desc = open(curdir + generalinfo + "%s%s" % (symbol.strip(), ".txt"))
+            else:
+                # print("trying to get desc from: " + curdir + generalinfo + desc_file)
+                desc = open(curdir + generalinfo + desc_file)
+        except IOError as e:
+            print("Missing descrption on element: " + symbol)
+            # print(e)
+        else:
+            desc_text = stripWikiCitations(desc.read().decode('utf-8', 'ignore'))
+            e.description = desc_text
+            db.session.add(e)
+            db.session.commit()
+        try:
+            triv_file = ''
+            for filename in os.listdir(curdir + triviapath):
+                if filename.startswith(symbol.strip()+".txt"):
+                    triv_file = filename
+            if triv_file == '':
+                trivia = open(curdir + triviapath + "%s%s" % (symbol.strip(), ".txt"))
+            else:
+                # print("trying to get desc from: " + curdir + triviapath + triv_file)
+                trivia = open(curdir + triviapath + triv_file)
+
+        except IOError as e:
+            print("Missing trivia for element: " + symbol)
+            # print(e)
+        else:
+            trivia_text = stripWikiCitations(trivia.read().decode('utf-8', 'ignore'))
+            if trivia_text is not None and trivia_text != "":
+                trivia_list = getTrivia(trivia_text)
+                for trivia_snippet in trivia_list:
+                    t = models.Trivia(description=trivia_snippet, element_number=i)
+                    db.session.add(t)
+                    db.session.commit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ db_generation/add_picture_data.py
@@ -0,0 +1,37 @@
+from app import db, models
+import os
+
+
+def main():
+    staticimagespath = "/static/images/"
+    curdir = "/home/elementalists/cs373-idb/app" + staticimagespath
+    default = "default"
+    for i in range(1, 119):
+        firstImageInFolder = True
+        e = models.Element.query.get(i)
+        element_name = e.element.strip()
+        element_dir_name = ''
+        print("element: " + element_name)
+        for filename in os.listdir(curdir):
+            # print("checking : " + filename)
+            if filename.lower().startswith(element_name.lower()):
+                print("Found Dir: " + curdir+filename)
+                element_dir_name = filename
+        if element_dir_name != '':
+            for imagefilename in os.listdir(curdir+element_dir_name):
+                print("--- Examining: " + imagefilename)
+                path = staticimagespath + element_dir_name + '/' + imagefilename
+                print(path)
+                imageType = None
+                if firstImageInFolder:
+                    print("-- default --")
+                    imageType = default
+                    firstImageInFolder = False
+                image = models.Image(image_path=path, element_number=i, image_type=imageType)
+                db.session.add(image)
+                db.session.commit()
+        else:
+            print("Could not find image folder for " + element_name)
+
+if __name__ == "__main__":
+    main()
