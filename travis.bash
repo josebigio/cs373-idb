843b453eeb98b11b236bebe9f5d455de9ad0656e
colindmurray <murray.colin43@gmail.com>
2015-04-20 20:47:40 -0500

    added coverage to requirements

--- travis.bash
+++ travis.bash
@@ -21,20 +21,7 @@ check_file "tests.out"
 echo "Running tests"
 
 coverage run tests.py 2> tests.out
-#coverage report -m FreeSpirits/*.py >> tests.out
-
-#echo "Generating Sphinx Documentation"
-#
-#rm -rf html doc
-#sphinx-apidoc . --full -o doc -H 'FreeSpirits' -A 'FreeSpirits Team' -V '1.0'
-#cd doc
-#rm -f conf.py index.rst
-#wget https://gist.githubusercontent.com/pybae/f600f8cdbc1f6a0ffe4e/raw/f1fee5a19962d20f77a30af0e120edca1f9c4b97/conf.py
-#wget https://gist.githubusercontent.com/pybae/701c26ed77093e825fbd/raw/080ad84dd9af27ee256cf71b854b05098bb46196/index.rst
-#
-#make html
-#mv _build/html ../html
-#cd ..
+coverage report -m app/*.py >> tests.out
 
 echo "Making IDB.log"
 
