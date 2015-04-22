084669e148722dbd08e033368f8c6e8ee18612da
Nathan Giles <nathanrgiles@gmail.com>
2015-04-21 22:27:14 -0500

    Further charts changes

--- app/static/scripts/elements_chart.js
+++ app/static/scripts/elements_chart.js
@@ -1,4 +1,3 @@
-/*
 var xmlhttp = new XMLHttpRequest();
 var url = "myTutorials.txt";
 
@@ -9,29 +8,45 @@ xmlhttp.onreadystatechange = function() {
 }
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
-*/
 
 temp = 72;
+gas_count = 0;
+liquid_count = 0;
+solid_count = 0;
+
+for (var key in element_info) {
+    if(element_info.hasOwnProperty(key)) {
+        if(temp > key[boiling_point_k]) {
+            gas_count++;
+        }
+        else if(temp > key[melting_point_k]) {
+            liquid_count++;
+        }
+        else {
+            solid_count++;
+        }
+    }
+}
 
 var data = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
+{
+    value: gas_count,
+    color:"#F7464A",
+    highlight: "#FF5A5E",
+    label: "Gas"
+},
+{
+    value: liquid_count,
+    color: "#46BFBD",
+    highlight: "#5AD3D1",
+    label: "Liquid"
+},
+{
+    value: solid_count,
+    color: "#FDB45C",
+    highlight: "#FFC870",
+    label: "Solid"
+}
 ]
 
 var options = {
@@ -43,6 +58,3 @@ window.onload = function(){
                 var ctx = document.getElementById("elements_chart").getContext("2d");
                 window.elements_chart = new Chart(ctx).Doughnut(data, options);
             };
-
-
-
