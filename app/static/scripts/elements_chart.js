d3b3208c815a8bd741185edc022dc1dc4ae1f12a
Nathan Giles <nathanrgiles@gmail.com>
2015-04-22 22:59:06 -0500

    Further charts changes

--- app/static/scripts/elements_chart.js
+++ app/static/scripts/elements_chart.js
@@ -31,7 +31,7 @@ var data = [
 
     window.onload = function(){
                     var ctx = document.getElementById("elements_chart").getContext("2d");
-                    window.elements_chart = new Chart(ctx).Doughnut(data, options);
+                    window.elements_chart = new Chart(ctx).Pie(data, options);
                 };
 
 xmlhttp.onreadystatechange = function() {
@@ -52,29 +52,18 @@ function construct_chart(elements_info) {
 
     for (var atomic_number in elements_info) 
     {
-        //if(elements_info.hasOwnProperty(atomic_number))
-        //{
-        for (var element_info in elements_info[atomic_number.toString()])
+        if(temp > elements_info[atomic_number.toString()]["boiling_point_k"]) 
         {
-           // if(elements_info.hasOwnProperty(element_info)) {
-           // document.write(element_info);
-           // document.write("\n");
-            //document.write(element_info["melting_point_k"].toString()+"\n");
-            if(temp > elements_info[atomic_number.toString()]["boiling_point_k"]) 
-            {
-                gas_count = gas_count + 1;
-            }
-            else if(temp > elements_info[atomic_number.toString()]["melting_point_k"]) 
-            {
-                liquid_count = liquid_count + 1;
-            }
-            else 
-            {
-                solid_count = solid_count + 1;
-            }
-            //}
+            gas_count = gas_count + 1;
         }
-        //
+        else if(temp > elements_info[atomic_number.toString()]["melting_point_k"]) 
+        {
+            liquid_count = liquid_count + 1;
+        }
+        else 
+        {
+            solid_count = solid_count + 1;
+        }
     }
 
     window.elements_chart.segments[0].value = gas_count;
