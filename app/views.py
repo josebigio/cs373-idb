bdeca6fcbeb1d6b8b05692d5d0aa5c2b58973846
Aizhuldyz Gabbassova <aigabbass90@gmail.com>
2015-04-23 18:07:35 -0500

    changed sql statement

--- app/views.py
+++ app/views.py
@@ -406,32 +406,33 @@ def handle_trivia(column_set):
 
 #search query result
 def perform_search(query):
-    statement = """SELECT atomic_number, symbol, element, name, period_number  FROM (SELECT atomic_number, symbol, element, g.name, period_number,
-    setweight(to_tsvector(CAST(atomic_number AS VARCHAR)),'A') ||
-              setweight(to_tsvector(element), 'A' )||
-      setweight(to_tsvector('simple', symbol), 'A')||
+    statement = """SELECT atomic_number, symbol, element, period_number, column_number, phase, most_stable_crystal, type, ionic_radius, electronegativity, first_ionization_potential, density, melting_point_k,
+ boiling_point_k, isotopes, year_of_discovery, specific_heat_capacity, electron_configuration, discoverer FROM (SELECT atomic_number, symbol, element, period_number, column_number, phase, most_stable_crystal, type, ionic_radius, electronegativity, first_ionization_potential, density, melting_point_k,
+ boiling_point_k, isotopes, year_of_discovery, specific_heat_capacity, electron_configuration, discoverer,
+	setweight(to_tsvector(CAST(atomic_number AS VARCHAR)),'A') ||
+			  setweight(to_tsvector(element), 'A' )||
+	  setweight(to_tsvector('simple', symbol), 'A')||
     setweight(to_tsvector(CAST(period_number AS VARCHAR)), 'D') ||
     setweight(to_tsvector(CAST(column_number AS VARCHAR)), 'D') ||
-                setweight(to_tsvector(phase), 'C') ||
-    setweight(to_tsvector('simple',most_stable_crystal),'C') ||
-                 setweight(to_tsvector(type), 'C') ||
-     setweight(to_tsvector(CAST(ionic_radius AS VARCHAR)), 'D') ||
-    setweight(to_tsvector(CAST(atomic_radius AS VARCHAR)), 'C') ||
-setweight(to_tsvector(CAST(electronegativity AS VARCHAR)), 'C') ||
-setweight(to_tsvector(CAST(first_ionization_potential AS VARCHAR)), 'C') ||
-          setweight(to_tsvector(CAST(density AS VARCHAR)), 'C') ||
-  setweight(to_tsvector(CAST(melting_point_k AS VARCHAR)), 'C') ||
-  setweight(to_tsvector(CAST(boiling_point_k AS VARCHAR)), 'C') ||
-         setweight(to_tsvector(CAST(isotopes AS VARCHAR)), 'C') ||
-            setweight(to_tsvector(CAST(year_of_discovery AS VARCHAR)), 'B') ||
-setweight(to_tsvector(CAST(specific_heat_capacity AS VARCHAR)), 'B') ||
-setweight(to_tsvector(CAST(electron_configuration AS VARCHAR)), 'C') ||
-setweight(to_tsvector(discoverer), 'A') ||
-setweight(to_tsvector(elements.description), 'C')
-    as document from elements
-    JOIN groups g ON elements.column_number = g.group_number) p_search
-    WHERE p_search.document @@ to_tsquery('{!s}')
-    ORDER BY ts_rank(p_search.document, to_tsquery('{!s}')) DESC;""".format(query, query)
+              setweight(to_tsvector(coalesce(string_agg(CAST(phase AS VARCHAR), ' '),'')), 'C') ||
+    setweight(to_tsvector('simple',coalesce(string_agg(CAST(most_stable_crystal AS VARCHAR), ' '), '')),'C') ||
+    			 setweight(to_tsvector('simple',coalesce(string_agg(CAST(type AS VARCHAR), ' '), '')), 'C') ||
+     setweight(to_tsvector('simple',coalesce(string_agg(CAST(ionic_radius AS VARCHAR), ' '), '')), 'D') ||
+    setweight(to_tsvector('simple',coalesce(string_agg(CAST(atomic_radius AS VARCHAR), ' '), '')), 'C') ||
+setweight(to_tsvector('simple',coalesce(string_agg(CAST(electronegativity AS VARCHAR), ' '), '')), 'C') ||
+setweight(to_tsvector('simple',coalesce(string_agg(CAST(first_ionization_potential AS VARCHAR), ' '), '')), 'C') ||
+		  setweight(to_tsvector(coalesce(string_agg(CAST(density AS VARCHAR), ' '), '')), 'C') ||
+  setweight(to_tsvector('simple', coalesce(string_agg(CAST(melting_point_k AS VARCHAR), ' '), '')), 'C') ||
+  setweight(to_tsvector('simple', coalesce(string_agg(CAST(boiling_point_k AS VARCHAR), ' '), '')), 'C') ||
+        setweight(to_tsvector('simple', coalesce(string_agg(CAST(isotopes AS VARCHAR), ' '), '')), 'C') ||
+         setweight(to_tsvector('simple', coalesce(string_agg(CAST(year_of_discovery AS VARCHAR), ' '), '')), 'B') ||
+setweight(to_tsvector('simple', coalesce(string_agg(CAST(specific_heat_capacity AS VARCHAR), ' '), '')), 'B') ||
+setweight(to_tsvector('simple',coalesce(string_agg(CAST(electron_configuration AS VARCHAR), ' '), '')), 'C') ||
+setweight(to_tsvector('simple', coalesce(string_agg(discoverer, ' '), '')), 'A')
+	as document from elements
+  group by atomic_number) p
+	WHERE p.document @@ to_tsquery('{{!s}}')
+	ORDER BY ts_rank(p.document, to_tsquery('{{!s}}')) DESC;""".format(query, query)
 
 
     result = db.engine.execute(statement).fetchall()
